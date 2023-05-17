117c6be7b141560bf6cda58edcd8c639bd90da4c
Slade <115507834+Slade1995@users.noreply.github.com>
2023-05-17 23:39:23 +1200

    Add files via upload

---
+++ week10/Phone.java
@@ -0,0 +1,17 @@
+package week10;
+
+public class Phone {
+	private String brand;
+
+	Phone (String brand){
+		this.brand=brand;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+}
